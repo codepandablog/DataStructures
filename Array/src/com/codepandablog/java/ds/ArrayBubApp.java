04610d250de88aa20ca429f748640491f2e3bce4
codepandablog <codepandablog@gmail.com>
2017-01-10 20:08:15 +0530

    Create ArrayBubApp.java

---
+++ Array/src/com/codepandablog/java/ds/ArrayBubApp.java
@@ -0,0 +1,25 @@
+package test1;
+
+public class ArrayBubApp {
+
+	public static void main(String arr[]){
+		ArrayBub array=new ArrayBub(100);
+		array.insert(100);
+		array.insert(12);
+		array.insert(15);
+		array.insert(-45);
+		array.insert(45);
+		array.insert(8);
+		
+		array.insert(10);
+		array.insert(12);
+		array.insert(1);
+		array.insert(-45);
+		array.insert(0);
+		array.insert(2);
+		array.display();
+		//array.bubbleSort();
+		array.selectionSort();
+		array.display();
+	}
+}
