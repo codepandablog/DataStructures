8a82e59bdd4413dfeac55c55b1f2cf90cf7becec
codepandablog <codepandablog@gmail.com>
2017-01-11 12:48:30 +0530

    Create ObjectSortApp.java

---
+++ Array/src/com/codepandablog/java/ds/ObjectSortApp.java
@@ -0,0 +1,24 @@
+package test1;
+
+public class ObjectSortApp {
+
+	public static void main(String[] args) {
+
+		ArrayInObj array=new ArrayInObj(100);
+		array.insert("Evans", "Patty", 24);
+		array.insert("Smith", "Doc", 59);
+		array.insert("Smith", "Lorraine", 37);
+		array.insert("Smith", "Paul", 37);
+		array.insert("Yee", "Tom", 43);
+		array.insert("Hashimoto", "Sato", 21);
+		array.insert("Stimson", "Henry", 29);
+		array.insert("Velasquez", "Jose", 72);
+		array.insert("Vang", "Minh", 22);
+		array.insert("Creswell", "Lucinda", 18);
+		System.out.println("Before sorting:");
+		array.display(); // display items
+		array.insertionSort();
+		array.display();
+	}
+
+}
